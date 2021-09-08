bc9ea4b30a40a7a5409eb3b4c26f99891e47e737
madan <madan@local.com>
2021-09-08 23:46:32 +0530

    added a program to calculate total stock value

--- src/com/inventory/JSONInventory.java
+++ src/com/inventory/JSONInventory.java
@@ -1,6 +1,5 @@
 package com.inventory;
 
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
---
+++ src/com/stockaccountmangement/StockAccountManagement.java
@@ -0,0 +1,110 @@
+package com.stockaccountmangement;
+
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.Writer;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Scanner;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+
+public class StockAccountManagement {
+
+	public static void main(String[] args) {
+		
+		writeJson();
+		calcTotalStock();
+	}
+	
+	private static void calcTotalStock() {
+		JSONParser jsonParser = new JSONParser();
+		try {
+			Reader reader = new FileReader("/Users/madanar/eclipse-workspace/YML training/employee.java/Object oriented programs/data/stock.json");
+			JSONObject jsonObject = (JSONObject) jsonParser.parse(reader);
+			reader.close();
+			JSONArray array = (JSONArray) jsonObject.get("stock");
+			Iterator iterator = array.iterator();
+			while(iterator.hasNext()) {
+				JSONObject jsonObject2 = (JSONObject) iterator.next();
+				System.out.println(jsonObject2);
+				String name = (String) jsonObject2.get("name");
+				Double n = (Double) jsonObject2.get("noOfShare");
+				Double sharePrice =  (Double) jsonObject2.get("sharePrice");
+				System.out.println("Name: " +name+ ", NoOfShare : " +n+ ", sharePrice : " +sharePrice);
+				outputStock(name, n ,sharePrice);
+			}
+			
+		}
+		catch(IOException | ParseException e)
+		{
+			e.printStackTrace();
+		}
+	}
+		
+	
+	private static void writeJson() {
+		JSONArray array = new JSONArray();
+		Scanner sc = new Scanner(System.in);
+		int choice = 1;
+		
+		while(choice == 1){
+			JSONObject object = new JSONObject();
+			System.out.println("Enter the name of the stock");
+			String name = sc.nextLine();
+			object.put("name", name);
+			System.out.println("Enter the number of stocks");
+			Double n = sc.nextDouble();
+			sc.nextLine();
+			object.put("noOfShare", n);
+			System.out.println("Enter the stock price");
+			Double sharePrice = sc.nextDouble();
+			sc.nextLine();
+			object.put("sharePrice", sharePrice);
+			
+			array.add(object);
+			
+			System.out.println("Do you want to add another object \n1.yes \n2.no");
+			choice = sc.nextInt();
+			sc.nextLine();
+		}
+		
+		JSONObject mainObject = new JSONObject();
+		mainObject.put("stock", array);
+		
+		try {
+			Writer writer = new FileWriter("/Users/madanar/eclipse-workspace/YML training/employee.java/Object oriented programs/data/stock.json");
+			writer.write(mainObject.toJSONString());
+			writer.flush();
+			writer.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	private static void outputStock(String name, Double n, Double sharePrice) {
+		JSONObject object = new JSONObject();
+		object.put("stockname", name);
+		object.put("stockValue", n*sharePrice);
+		
+		try {
+			Writer writer = new FileWriter("/Users/madanar/eclipse-workspace/YML training/employee.java/Object oriented programs/data/totalStock.json", true);
+			writer.write(object.toJSONString());
+			writer.flush();
+			writer.close();
+			System.out.println("output is successfully wirtten into a file");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+}
